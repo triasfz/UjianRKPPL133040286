0c1b3365ab4623a5bb14d1ccaa0cbb14bbf790d8
rizqon <rizqon.sidik.maulana@gmail.com>
2016-05-13 17:03:01 +0700

    Ujian Praktikum RKPPL pendeklarasian logger

--- src/com/artivisi/quickstart/log4j/HelloLog.java
+++ src/com/artivisi/quickstart/log4j/HelloLog.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 public class HelloLog {
 
 	public static void main(String[] args) {
-		Logger Logger = Logger.getLogger(HelloLog.class);
+		private static final Logger Logger = Logger.getLogger(HelloLog.class);
 		
 		Logger.info("Ini adalah debug message : Contoh Log4J");
 		Logger.warning("Ini adalah debug message : Contoh Log4J");
